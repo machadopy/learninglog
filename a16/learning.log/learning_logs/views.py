c990d523af0f0f9caf051e747d3effd6ad00c5b7
machadopy <vitorandroidcwb@gmail.com>
2025-09-11 02:52:50 -0300

    commit

---
+++ a16/learning.log/learning_logs/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+# Create your views here.
+
+def index(request):
+    '''pagina principal do learning Logs'''
+    return render(request,'learning_logs/index.html')
+
+
+def topics(request):
+    '''todos os Topicos'''
+    topics = Topic.objects.order_by('date_added')
+    entries = Entry.objects.order_by('date_added')
+    context = {'topics': topics, 'entries': entries}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    '''mostra um topico'''
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    '''adiciona novo assunto'''
+    if request.method != 'POST':
+        #NENHUM DADO CRIA FORM EM BRANCO
+        form =  TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+    
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+     
+    
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    '''edita entradas'''
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic.id]))
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
+
+
+def delete_entry(request, entry_id):
+    '''deleta entradas'''
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method == 'POST':
+        entry.delete()
+        return HttpResponseRedirect(reverse('topic', args=[topic.id]))
+
+    context = {'entry': entry, 'topic': topic}
+    return render(request, 'learning_logs/delete_entry.html', context)
---
+++ learning.log/learning_logs/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+from django.http import HttpResponseRedirect, Http404
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def index(request):
+    '''pagina principal do learning Logs'''
+    return render(request,'learning_logs/index.html')
+
+@login_required
+def topics(request):
+    '''todos os Topicos'''
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+    entries = Entry.objects.filter(topic__owner=request.user).order_by('date_added')
+    context = {'topics': topics, 'entries': entries}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+@login_required
+def topic(request, topic_id):
+    '''mostra um topico'''
+    topic = Topic.objects.get(id = topic_id)
+    if topic.owner !=request.user:
+        raise Http404
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+
+@login_required
+def new_topic(request):
+    '''adiciona novo assunto'''
+    if request.method != 'POST':
+        #NENHUM DADO CRIA FORM EM BRANCO
+        form =  TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+    
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+     
+@login_required    
+def new_entry(request, topic_id):
+    topic = Topic.filter(owner=request.user).objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+@login_required
+def edit_entry(request, entry_id):
+    '''edita entradas'''
+    entry = Entry.filter(owner=request.user).objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic.id]))
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
+
+@login_required
+def delete_entry(request, entry_id):
+    '''deleta entradas'''
+    entry = Entry.filter(owner=request.user).objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method == 'POST':
+        entry.delete()
+        return HttpResponseRedirect(reverse('topic', args=[topic.id]))
+
+    context = {'entry': entry, 'topic': topic}
+    return render(request, 'learning_logs/delete_entry.html', context)
